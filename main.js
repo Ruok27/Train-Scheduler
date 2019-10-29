626c3c38839119003439b52810f173cef4e32f7f
ruok27 <jakxii27@gmail.com>
2019-10-29 19:17:29 -0400

    setting up

---
+++ main.js
@@ -0,0 +1,27 @@
+let tim = $("#test");
+
+
+$( document ).ready(function() {
+
+
+tim.css("color","green");
+
+});
+
+
+
+$("#test").on("click", function(){
+
+
+    tim.css("color","red");
+
+
+
+
+
+
+
+})
+
+
+
