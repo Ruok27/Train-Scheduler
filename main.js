606aae41ee80fe6509232b17b71f6c2564b85e0e
ruok27 <jakxii27@gmail.com>
2019-11-01 02:34:52 -0400

    just need to integrate firebase

--- main.js
+++ main.js
@@ -1,27 +1,75 @@
-let tim = $("#test");
+const tBody = $("<tbody>");
+const tRow = $("<tr>");
+const tHeader = $("<th>");
+const tData = $("<td>");
 
-
-$( document ).ready(function() {
-
-
-tim.css("color","green");
-
-});
+let table = $(".table");
 
 
 
-$("#test").on("click", function(){
-
-
-    tim.css("color","red");
 
 
 
+let nameInput;
+let destination;
+let trainTime;
+let frequency;
+
+
+
+
+
+
+
+
+$("#submitButton").on("click", function (event) {
+    event.preventDefault();
+
+
+
+
+
+
+    nameInput = $("#TrainName").val().trim();
+    destination = $("#Destination").val().trim();
+    trainTime = $("#Train-Time").val().trim();
+    frequency = $("#Frequency").val().trim();
+
+    let convertedTime = moment(trainTime, "HH:mm");
+    let minutesAway = convertedTime.add(frequency, 'minutes').format('HH:mm');
+
+
+
+    let newRow = $("<tr>").append(
+
+        $("<th>").text(nameInput),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(trainTime),
+        $("<td>").text(minutesAway));
+
+
+
+    $(table).append(newRow);
+
+    nameInput = "";
+    destination = "";
+    trainTime = "";
+    frequency = "";
+    convertedTime = "";
+    minutesAway = "";
+
+
+    $("#TrainName").val("");
+    $("#Destination").val("");
+    $("#Train-Time").val("");
+    $("#Frequency").val("");
 
 
 
 
 })
+
 
 
 
